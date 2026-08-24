a06a3cb346144c50a9a91690c7170420859e3126
Akshay <services.techxit@gmail.com>
2026-08-24 12:14:56 +0530

    Revert frontend folder to dist per user request

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingRoutes);
 
 // Serve static frontend files from 'public' folder
-app.use(express.static(path.join(__dirname, '../client-dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // Catch-all route to serve index.html for client-side routing
 app.get('*', (req, res) => {
