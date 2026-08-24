7700cbf0d31739b3b8a21708289225a11c0630d5
Akshay <services.techxit@gmail.com>
2026-08-24 14:43:04 +0530

    Fix catch-all route to point to dist instead of public

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 // Catch-all route to serve index.html for client-side routing
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
